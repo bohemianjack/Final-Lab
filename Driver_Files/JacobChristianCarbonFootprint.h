fbf6cbc54741bb82d3b802f4b6707bc99b646163
bohemianjack <34169463+bohemianjack@users.noreply.github.com>
2018-12-12 20:08:06 -0600

    Update JacobChristianCarbonFootprint.h

--- Driver_Files/JacobChristianCarbonFootprint.h
+++ Driver_Files/JacobChristianCarbonFootprint.h
@@ -1,18 +1,16 @@
-// Author:				Jacob Christian
-// Assignment Number:   Exam 3, part 2
-// File Name:			JacobChristianCarbonFootprint.h
-// Course/Section:      COSC 1337 Section 014
-// Date:                December 13th, 2018
-// Instructor:          Dr. Bernard Ku
-
 #ifndef CARBONFOOTPRINT_H
 #define CARBONFOOTPRINT_H
-
+#include <iostream>
+using namespace std;
 class CarbonFootPrint
 {
 
 public:
     virtual void getCarbonFootPrint() = 0;
+    //Virtual destructor to prevent undefined behavior,
+    //guarantees all constructors are properly destroyed.
+    virtual ~CarbonFootPrint(){}
+
 
 
 };
